f4a05bcb686bf094972026cbe746e96e5ae51907
Amit <amitgupta561710@gmai.com>
2025-05-17 23:30:25 +0530

    Initializing-project

---
+++ app/(tabs)/index.tsx
@@ -0,0 +1,9 @@
+import { Text, View } from "react-native";
+
+export default function Index() {
+  return (
+    <View className="flex-1 flex-col justify-center gap-5 items-center">
+      <Text className="text-dark-200 text-5xl">Welcome!</Text>
+    </View>
+  );
+}
